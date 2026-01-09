80a5e111252a5d10ab4a086ca0eff6a76fbc0e13
Desire Banyeretse <banyerdes@gmail.com>
2026-01-08 20:45:42 -0600

    Updated default user and fixed SSH key

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,5 +1,11 @@
 # modules/compute/main.tf
 
+# --- Key Generation (Internal Cluster Key) ---
+resource "tls_private_key" "lab_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 # --- AMI Lookup ---
 data "aws_ami" "rhel8" {
   most_recent = true
@@ -25,29 +31,48 @@ resource "aws_instance" "control" {
   tags = { Name = "ansible-control" }
 
   # Cloud-Init:
-  # 1. Rename default user to 'hyfer'
-  # 2. Enable root login by copying authorized keys from hyfer
-  # 3. Inject the Private Key into /root/.ssh/id_rsa using Base64
-  # 4. Set FQDN Hostname (hyfer.com)
+  # 1. Create 'hyfer' user
+  # 2. Copy YOUR key (from ec2-user) to hyfer and root
+  # 3. Add INTERNAL GENERATED public key to root authorized_keys (for inter-node comms)
+  # 4. Inject INTERNAL GENERATED private key into /root/.ssh/id_rsa
   user_data = <<-EOF
     #cloud-config
-    system_info:
-      default_user:
-        name: hyfer
+    users:
+      - default
+      - name: hyfer
+        groups: [wheel]
+        sudo: ["ALL=(ALL) NOPASSWD:ALL"]
+        shell: /bin/bash
+
     hostname: ansible-control.hyfer.com
     fqdn: ansible-control.hyfer.com
     preserve_hostname: true
     manage_etc_hosts: true
-    disable_root: false
     ssh_pwauth: true
+    disable_root: false
+
     runcmd:
       - [ sh, -c, "hostnamectl set-hostname ansible-control.hyfer.com" ]
-      - [ sh, -c, "cp /home/hyfer/.ssh/authorized_keys /root/.ssh/" ]
+      
+      # --- Setup Hyfer User (Your Key) ---
+      - [ sh, -c, "mkdir -p /home/hyfer/.ssh" ]
+      - [ sh, -c, "cp /home/ec2-user/.ssh/authorized_keys /home/hyfer/.ssh/" ]
+      - [ sh, -c, "chown -R hyfer:hyfer /home/hyfer/.ssh" ]
+      - [ sh, -c, "chmod 700 /home/hyfer/.ssh" ]
+      - [ sh, -c, "chmod 600 /home/hyfer/.ssh/authorized_keys" ]
+      - [ sh, -c, "cp /home/ec2-user/.ssh/authorized_keys /root/.ssh/" ]
+      
+      # --- Add Internal Cluster Public Key to Root ---
+      - [ sh, -c, "echo '${tls_private_key.lab_key.public_key_openssh}' >> /root/.ssh/authorized_keys" ]
+      
+      # --- Final Permissions & Contexts ---
       - [ sh, -c, "chmod 600 /root/.ssh/authorized_keys" ]
       - [ sh, -c, "chown root:root /root/.ssh/authorized_keys" ]
+      - [ sh, -c, "restorecon -R /root/.ssh /home/hyfer/.ssh" ]
+
     write_files:
       - encoding: b64
-        content: ${base64encode(var.AWS_SSH_KEY_CONTENT)}
+        content: ${base64encode(tls_private_key.lab_key.private_key_pem)}
         path: /root/.ssh/id_rsa
         permissions: '0600'
         owner: root:root
@@ -67,19 +92,37 @@ resource "aws_instance" "managed" {
 
   user_data = <<-EOF
     #cloud-config
-    system_info:
-      default_user:
-        name: hyfer
+    users:
+      - default
+      - name: hyfer
+        groups: [wheel]
+        sudo: ["ALL=(ALL) NOPASSWD:ALL"]
+        shell: /bin/bash
+
     hostname: ansible${count.index + 2}.hyfer.com
     fqdn: ansible${count.index + 2}.hyfer.com
     preserve_hostname: true
     manage_etc_hosts: true
     disable_root: false
+
     runcmd:
       - [ sh, -c, "hostnamectl set-hostname ansible${count.index + 2}.hyfer.com" ]
-      - [ sh, -c, "cp /home/hyfer/.ssh/authorized_keys /root/.ssh/" ]
+      
+      # --- Setup Hyfer User (Your Key) ---
+      - [ sh, -c, "mkdir -p /home/hyfer/.ssh" ]
+      - [ sh, -c, "cp /home/ec2-user/.ssh/authorized_keys /home/hyfer/.ssh/" ]
+      - [ sh, -c, "chown -R hyfer:hyfer /home/hyfer/.ssh" ]
+      - [ sh, -c, "chmod 700 /home/hyfer/.ssh" ]
+      - [ sh, -c, "chmod 600 /home/hyfer/.ssh/authorized_keys" ]
+      - [ sh, -c, "cp /home/ec2-user/.ssh/authorized_keys /root/.ssh/" ]
+
+      # --- Add Internal Cluster Public Key to Root ---
+      - [ sh, -c, "echo '${tls_private_key.lab_key.public_key_openssh}' >> /root/.ssh/authorized_keys" ]
+
+      # --- Final Permissions & Contexts ---
       - [ sh, -c, "chmod 600 /root/.ssh/authorized_keys" ]
       - [ sh, -c, "chown root:root /root/.ssh/authorized_keys" ]
+      - [ sh, -c, "restorecon -R /root/.ssh /home/hyfer/.ssh" ]
   EOF
 }
 
@@ -95,19 +138,37 @@ resource "aws_instance" "db_node" {
 
   user_data = <<-EOF
     #cloud-config
-    system_info:
-      default_user:
-        name: hyfer
+    users:
+      - default
+      - name: hyfer
+        groups: [wheel]
+        sudo: ["ALL=(ALL) NOPASSWD:ALL"]
+        shell: /bin/bash
+
     hostname: ansible5.hyfer.com
     fqdn: ansible5.hyfer.com
     preserve_hostname: true
     manage_etc_hosts: true
     disable_root: false
+
     runcmd:
       - [ sh, -c, "hostnamectl set-hostname ansible5.hyfer.com" ]
-      - [ sh, -c, "cp /home/hyfer/.ssh/authorized_keys /root/.ssh/" ]
+      
+      # --- Setup Hyfer User ---
+      - [ sh, -c, "mkdir -p /home/hyfer/.ssh" ]
+      - [ sh, -c, "cp /home/ec2-user/.ssh/authorized_keys /home/hyfer/.ssh/" ]
+      - [ sh, -c, "chown -R hyfer:hyfer /home/hyfer/.ssh" ]
+      - [ sh, -c, "chmod 700 /home/hyfer/.ssh" ]
+      - [ sh, -c, "chmod 600 /home/hyfer/.ssh/authorized_keys" ]
+      - [ sh, -c, "cp /home/ec2-user/.ssh/authorized_keys /root/.ssh/" ]
+
+      # --- Add Internal Cluster Public Key to Root ---
+      - [ sh, -c, "echo '${tls_private_key.lab_key.public_key_openssh}' >> /root/.ssh/authorized_keys" ]
+
+      # --- Final Permissions & Contexts ---
       - [ sh, -c, "chmod 600 /root/.ssh/authorized_keys" ]
       - [ sh, -c, "chown root:root /root/.ssh/authorized_keys" ]
+      - [ sh, -c, "restorecon -R /root/.ssh /home/hyfer/.ssh" ]
   EOF
 }
 
